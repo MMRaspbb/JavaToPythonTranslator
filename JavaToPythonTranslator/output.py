3b332e0554a97899639ad2b2801e3c39e235691a
MMRasbb <maciek.malinowski.poczta@gmail.com>
2025-01-23 12:27:54 +0100

    "adding output"

---
+++ JavaToPythonTranslator/output.py
@@ -0,0 +1,15 @@
+class OuterClass:
+    @staticmethod
+    def ifElse(a):
+        if a > 10:
+            print(">10")
+        elif a > 8:
+            print(">8")
+        elif a > 6:
+            print(">6")
+        elif a > 4:
+            print(">4")
+        elif a > 2:
+            print(">2")
+        else:
+            print("<=2")
